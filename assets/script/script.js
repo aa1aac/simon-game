77879fc042d6ee32ecf0cdee796bed9cfba33fc0
aa1aac <ach.aayush.11@gmail.com>
2018-06-03 22:02:18 +0545

    update

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,4 +1,72 @@
-// defination of simon object for game body
+// variables for game defined
+ var simonSeq=[0,1,2,3]; //stores random sequence generated
+ var userSeq=[]; //stores input by the user
+ var id=0;
+ var color=0;
+ var level=0;
+ var boardSound=[
+  'https://s3.amazonaws.com/freecodecamp/simonSound1.mp3',// for red 0 
+  'https://s3.amazonaws.com/freecodecamp/simonSound2.mp3', //yelllow
+  'https://s3.amazonaws.com/freecodecamp/simonSound3.mp3', //green
+  'https://s3.amazonaws.com/freecodecamp/simonSound4.mp3'  //blue
+ ]
+ // start the game when window is loaded
+$(document).ready(function(){
+  // add event listener to the start button
+  $('.start').click(function(){
+    level++;
+      startSequence();
+  })
+
+  // click for input 
+  $(".simon").click(function()
+  {
+    id= $(this).attr("id");
+    color=$(this).attr("class").split(" ")[1];
+    addClassSound(id,color);
+    userSeq.push(id);
+  })
+})
+
+
+
+// defination of startSequence function
+function startSequence(){
+  console.log(level);
+   $(".count").text(level);
+  //  getRandomNum();
+   var i=0;
+   var interval=setInterval(function(){
+     id= simonSeq[i];
+     color=$('#'+id).attr('class').split(" ")[1];
+     console.log(id+" "+color);
+     addClassSound(id,color);
+     if (i=simonSeq.length){
+        clearInterval(interval);
+     }
+   },1000);
+}
+
+// function to get random number for index;
+ function getRandomNum(){
+   var num=Math.floor(Math.random()*4);
+   simonSeq.push(num);
+ }
+
+ // function for class sound
+ function addClassSound(id,color){
+  $("#"+id).addClass(color+"-active");
+  playSound(id);
+  setTimeout(function(){
+       $("#"+id).removeClass(color+"-active");
+  },200)
+ }
+
+ //play sound function
+ function playSound(id){
+
+ }
+/*// defination of simon object for game body
 var simon={
  count:0,
  colorPossibilities :["red", "yellow", "green", "blue"],
@@ -14,12 +82,12 @@ var simon={
   },
 }
 $(".start").click(function (){
-  setTimeout(game(),2000);
+  setTimeout(game(),1000);
  
 })
 $(".strict").click(function(){
     simon.strictMode=true;
-    setTimeout(game(), 2000);
+    setTimeout(game(), 1000);
 })
 function game() {
     gameStart();
@@ -119,12 +187,13 @@ function playerTurn(arr){
           alert('Wrong move! Try again!');
           displayMoves();
         }
-       } else {
+       } 
+       else {
           console.log('Good Move!');
-          playAudio(simon.sound.arr);
-          var check = game.movesByPlayer.length === game.currentGame.length;
+           playAudio(simon.sound.arr)
+          var check = simon.movesByPlayer.length === simon.randomMoves.length;
           if (check) {
-            if(game.count == 20){
+            if(simon.count == 20){
               alert('You won! Congrats.');
             } else {
               alert('Next round!');
@@ -135,4 +204,4 @@ function playerTurn(arr){
 }
 function nextLevel(){
     count();
-}
+}*/
